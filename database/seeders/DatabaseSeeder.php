03972878322871a979cc6f53385e81cb8b037744
Priscilla <priscilla.carmo@bwtech.com>
2024-06-20 20:38:33 -0300

    Add migrations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PrestadorSeeder::class,
+            ServicosTableSeeder::class,
+            ServicoPrestadorSeeder::class,
         ]);
     }
 }
